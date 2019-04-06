70330b46b4c1a2f73b676fa9d0749c4c8dedcaa5
Shahadan Bin Saad <shahadan@uitm.edu.my>
2019-04-06 21:06:57 +0800

    Variable vs Constant

---
+++ 210/vs.php
@@ -0,0 +1,22 @@
+<?
+  $title = "Bismillah";
+  $mula="Dengan Nama Allah Yang Maha Pemurah Lagi Maha Mengasihani";
+
+  define("AUTHOR", "Shahadan Bin Saad");
+
+?>
+
+<html>
+	<head>
+		<title>
+			<?=$title?>
+		</title>
+	</head>
+	<body>
+    <div align="center">
+			<h1><?=$mula?></h1>
+      Quadrillion Terima Kasih
+    </div>
+    <div align="right">ikhlas dari <?=AUTHOR?></div>
+	</body>
+</html>
