1774a28a07cb6211090d34a9f7bed25874299fe7
Charlie Root <root@F112PHP.nadahahs.32bits>
2019-04-06 18:25:03 +0800

    modified:   index.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<html><head><title></title></head>
-<body>
+<?
+	$title = "Belajar PHP";
+?>
+
+<html>
+   <head>
+      <title><?=$title?></title>
+   </head>
+
+   <body>
 
 <?
 	$sir = "Shahadan Bin Saad";
 ?>
-<div align="center">
-<h1><?=$sir?></h1>
-</div>
-</body>
+      <div align="center">
+         <h1><?=$sir?></h1>
+      </div>
+   </body>
 </html>
