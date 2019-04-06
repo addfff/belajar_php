370163e3ef1a9acabc0f654f0d19528ab4504328
Shahadan Bin Saad <shahadan@uitm.edu.my>
2019-04-06 20:55:45 +0800

    ulangkaji 210

---
+++ 210/index.php
@@ -0,0 +1,18 @@
+<?
+  $title = "Bismillah"
+  $mula="Dengan Nama Allah Yang Maha Pemurah Lagi Maha Mengasihani";
+
+?>
+
+<html>
+	<head>
+		<title>
+			<?=$title?>
+		</title>
+	</head>
+	<body>
+    <div align="center">
+			<h1><$mula></h1>
+    </div>
+	</body>
+</html>
