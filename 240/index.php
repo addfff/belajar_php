f3c2b88de0705a53c77be119001e280fc0901936
User User2 <user2@F112PHP.nadahahs.32bits>
2019-04-07 21:33:15 +0800

    function call many time

--- 240/index.php
+++ 240/index.php
@@ -1,11 +1,12 @@
 <?
 
-	function thanksGod(){
-		echo "Alhamdulillah";
+	function thanksGod($begin){
+		echo "$begin. Alhamdulillah.<br>";
 	}
 
-	echo "Syukur ";
-	thanksGod();
-
+	thanksGod("Syukur");
+	thanksGod("Terima Kasih");
+	thanksGod("Dah siap");
+	thanksGod("Akhirnya sampai juga");
 
 ?>
