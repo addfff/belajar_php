b5129ccd9bbee12fe9a2e6266ed7b5e622620e77
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-08 21:53:47 +0800

    pemurah & pengasih

--- 260/associatives.php
+++ 260/associatives.php
@@ -1,7 +1,13 @@
 <?
 
-	$associativesArray = array("Key"=>"Value");
-	echo $associativesArray["Key"];
+	$name99 = array(
+		"Ar Rahman"=>"Maha Pemurah",
+		"Ar Rahim"=>"Maha Penyayang",
+		"Al Malik"=>"Maha Menguasai"
+	);
+	
+	$name99["Ar Rahman"]="Maha Pengasih";
+	echo $name99["Ar Rahman"];
 
 ?>
 
