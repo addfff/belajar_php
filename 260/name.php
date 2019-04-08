38bd98f25b5fe90d3e6aa2651d157302d5253d15
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-08 10:16:17 +0800

    add to array Al Quddus

--- 260/name.php
+++ 260/name.php
@@ -1,11 +1,15 @@
 <?
 	$name = array("Ar Rahman","Ar Rahim","Al Malik");
+	$name[3] = "Al Quddus";
+	$name[6] = "Al Muhaimin";
 
-	//echo count($name);
-
-	for ($x = 0; $x < 3; $x++){
+	echo count($name);
+/*
+	for ($x = 0; $x < 6; $x++){
 	        echo "<br>";
         	echo $name[$x];
 	}
-
+*/
+	echo "<br>";
+	echo $name[6];
 ?>
