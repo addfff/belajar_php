2cdefd905dcff4fa222a7c4dc12ed9537ed1959a
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-08 23:09:26 +0800

    class with 2 objects: harini & esok

--- 280/index.php
+++ 280/index.php
@@ -13,7 +13,16 @@ $harini->bila = date("d-m-Y");
 $harini->katmana = "Dewan Perdana";
 $harini->isBoleh = true;
 
-echo $harini->isBoleh;
+//echo $harini->isBoleh;
+
+$d = strtotime("tomorrow");
+$esok = new Janji;
+$esok->bil = 2;
+$esok->bila = date("d-m-Y", $d);
+$esok->katmana = "Tempat Biasa";
+$esok->isBoleh = "insyaAllah";
+
+echo $esok->bila;
 
 
 
