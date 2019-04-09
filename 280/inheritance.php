603b0cfc62644a56a95d50c85446349f61e33346
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-10 00:27:21 +0800

    has Inheritance

---
+++ 280/inheritance.php
@@ -0,0 +1,23 @@
+<?
+require 'janji.php';
+
+class JanjiMelayu extends Janji {
+
+	function hasInheritance(){
+		echo "<br>Yes";
+	}
+}
+
+$d = strtotime("first day of -1 month");
+$e = "RNR";
+
+$nantilah = new JanjiMelayu(1, date("d-m-Y", $d), $e, 1);
+
+$nantilah->hasInheritance();
+echo "<br>$nantilah->bil, $nantilah->bila, $nantilah->katmana, $nantilah->isBoleh";  
+$nantilah->setBoleh(false);
+$r = $nantilah->getBoleh();
+$s = $nantilah->hasBoleh();
+echo "<br>$s ($r)";
+?>
+
---
+++ 280/janji.php
@@ -0,0 +1,36 @@
+<?
+
+class Janji {
+	public $bil;
+	public $bila;
+	public $katmana;
+	public $isBoleh;
+			function __construct($a,$b,$c,$d){
+				$this->bil = $a;
+				$this->bila = $b;
+				$this->katmana = $c;
+				$this->isBoleh = $d;
+	}
+
+	function hasBoleh(){
+		if($this->isBoleh)
+			return "InsyaAllah";
+		else
+			return "Innalillah";
+	}
+
+	function getBoleh(){
+			return $this->isBoleh;
+	}
+
+	function setBoleh($newBoleh){
+		if($newBoleh)
+			$this->isBoleh = $newBoleh;
+		else if(!$newBoleh)
+			$this->isBoleh = $newBoleh;
+		else
+			echo "Sila masukkan true/false sahaja";
+	}		
+}
+
+?>
