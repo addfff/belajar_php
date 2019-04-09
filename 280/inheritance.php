2cd1dce6029dc94e53c6af9af7b2c7c4c8f76312
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-10 00:49:27 +0800

    Sila Fix Tambahan Masa Satu Jam sebab Janji Melayu

--- 280/inheritance.php
+++ 280/inheritance.php
@@ -3,15 +3,26 @@ require 'janji.php';
 
 class JanjiMelayu extends Janji {
 
+	public $masa;
+	private $tambahan;
+
+	function setMasa($newMasa){
+		$this->tambahan = time($newMasa) + (10 * 60 * 60);
+		echo "<br> $this->tambahan";
+		$this->masa = date($newMasa, strtotime("+1 hour"));
+	}
+
 	function hasInheritance(){
 		echo "<br>Yes";
 	}
 }
 
-$d = strtotime("first day of -1 month");
+$d = strtotime("3pm first day of -1 month");
 $e = "RNR";
 
 $nantilah = new JanjiMelayu(1, date("d-m-Y", $d), $e, 1);
+$nantilah->setMasa(date("H:i",$d));
+echo "<br>$nantilah->masa";
 
 $nantilah->hasInheritance();
 echo "<br>$nantilah->bil, $nantilah->bila, $nantilah->katmana, $nantilah->isBoleh";  
